fbc01f6f6cb757c1d65ce6716a79a796cf7b0e39
Jack Roden <jack.roden412@gmail.com>
2026-03-06 19:55:38 +0000

    update to take input

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/jroden2/stackforge/pkg/bundle"
+	"github.com/jroden2/stackforge/pkg/installer"
 )
 
 func main() {
@@ -21,6 +26,29 @@ func main() {
 		for i, b := range bundles {
 			fmt.Printf("%d %s\n", i+1, b.Title)
 			fmt.Printf("  %s\n", b.Description)
+		}
+
+		fmt.Println("Choose a bundle:")
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+
+		input = strings.TrimSpace(input)
+
+		selection, err := strconv.Atoi(input)
+		if err != nil {
+			fmt.Println("Invalid selection")
+			return
+		}
+
+		index := selection - 1
+		if index < 0 || index >= len(bundles) {
+			fmt.Println("Invalid selection")
+			return
+		}
+
+		err = installer.InstallBundle(bundles[index])
+		if err != nil {
+			fmt.Println("Installation failed:", err)
 		}
 	}
 }
