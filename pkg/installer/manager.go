df927d0d1a4859b33388755d1b8c68ba320ccda9
Jack Roden <jack.roden412@gmail.com>
2026-03-06 16:32:45 +0000

    updated manager, created curl and brew files for authoring

--- pkg/installer/manager.go
+++ pkg/installer/manager.go
@@ -5,4 +5,7 @@ import "github.com/jroden2/stackforge/pkg/domain"
 type Manager interface {
 	Name() string
 	Install(pkg domain.Package) error
+	IsInstalled(pkg domain.Package) bool
+	Upgrade(pkg domain.Package) error
+	Uninstall(pkg domain.Package) error
 }
---
+++ pkg/installer/managers/brew.go
@@ -0,0 +1,4 @@
+package managers
+
+type brewManager struct {
+}
