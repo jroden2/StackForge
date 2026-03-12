7aa3d302041860d379f7d69ce892c6605ec95fc5
Jack. <95051977+jroden2@users.noreply.github.com>
2026-03-12 12:21:56 +0000

    Update package.go
    fix incorrect placed speech marks

--- pkg/domain/package.go
+++ pkg/domain/package.go
@@ -1,8 +1,8 @@
 package domain
 
 type Package struct {
-	ID           string       `"json:id"`
-	Name         string       `"json:name"`
-	Manager      string       `"json:manager"`
-	InstallLogic InstallLogic `"json:installLogic"`
+	ID           string       `json:"id"`
+	Name         string       `json:"name"`
+	Manager      string       `json:"manager"`
+	InstallLogic InstallLogic `json:"installLogic"`
 }
